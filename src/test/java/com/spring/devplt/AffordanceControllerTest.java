28dcfa89dd99454074c76a5ab0b73ef3e728bc41
TrulyNotMalware <notleebutyee@gmail.com>
2022-04-29 17:01:17 +0900

    test : Add Affordance Hypermedia test source
    - test affordance with unittest.
    no related issue

---
+++ src/test/java/com/spring/devplt/AffordanceControllerTest.java
@@ -0,0 +1,53 @@
+package com.spring.devplt;
+
+import com.spring.devplt.controllers.AffordanceHypermediaController;
+import com.spring.devplt.models.User;
+import com.spring.devplt.repository.UserRepository;
+import com.spring.devplt.services.Services;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(controllers = AffordanceHypermediaController.class)
+public class AffordanceControllerTest {
+    @Autowired private WebTestClient webTestClient;
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean private Services services;
+
+    @BeforeEach
+    public void setup(){
+        //Test data 준비.
+        User sampleUser = new User("root","1234","Admin",true,null);
+
+        //상호작용 정의.
+        when(this.userRepository.findById("root"))
+                .thenReturn(Mono.just(sampleUser));
+        when(userRepository.save(any(User.class))).thenReturn(Mono.just(sampleUser));
+
+        this.services = new Services(this.userRepository,null);
+    }
+
+    @Test
+    void findUser(){
+        this.webTestClient
+                .get().uri("/api/hypermedia/affordance/users/root")
+                .accept(MediaTypes.HAL_FORMS_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .consumeWith(stringEntityExchangeResult -> {
+                    assertThat(stringEntityExchangeResult.getResponseBody()).contains("links");
+                    System.out.println(stringEntityExchangeResult.getResponseBody());
+                });
+    }
+}
