882d8757771db84d8fbe9d1156def067928aa076
TrulyNotMalware <notleebutyee@gmail.com>
2022-07-07 17:47:09 +0900

    feat : Spring Securiy config settings
    - Add UserRoles in Usermodel
    - change deprecatedmethod ( withDefaultPasswordEncoder )
    - Add UserDetailsService
    withDefaultPasswordEncoder chain is deprecated.

--- src/main/java/com/spring/devplt/repository/UserRepository.java
+++ src/main/java/com/spring/devplt/repository/UserRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, String>, ReactiveQueryByExampleExecutor<User> {
 
+    Mono<User> findByName(String name);
 }
--- src/main/java/com/spring/devplt/security/SecurityConfig.java
+++ src/main/java/com/spring/devplt/security/SecurityConfig.java
@@ -1,29 +1,75 @@
 package com.spring.devplt.security;
 
+import com.spring.devplt.models.User;
+import com.spring.devplt.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationProvider;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${jwt.secret}")
     private String SECRET_KEY;
 
+    @Bean
+    public ReactiveUserDetailsService userDetailsService(UserRepository repository){
+        return username -> {
+            Mono<User> user = repository.findByName(username);
+            if (user == null) return Mono.empty();
+            return user.map(user1 -> {
+                log.warn("User.withDefaultPasswordEncoder() is considered unsafe for production and is only intended for sample applications.");
+                PasswordEncoder encoder = passwordEncoder();
+                return org.springframework.security.core.userdetails.User.withUsername(user1.getName())
+                        .passwordEncoder(encoder::encode)
+                        .password(user1.getPwd())
+                        .build();
+            });
+        };
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+//    @Bean
+//    public ReactiveUserDetailsService userDetailsService(UserRepository repository){
+//        return username -> repository.findByName(username)
+//                .map(user -> User.withDefaultPasswordEncoder())
+//                DEPRECATED
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.cors().and().csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests().antMatchers("/api/test","/").permitAll()
                 .anyRequest().authenticated();
-
     }
 
 //    @Override
