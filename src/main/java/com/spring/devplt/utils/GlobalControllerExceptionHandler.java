a2a78cec691fd5dc7437243c68c49c44c1871126
TrulyNotMalware <notleebutyee@gmail.com>
2022-09-02 16:31:43 +0900

    Remove unused class, Modify ResponseEntity, Handler to Mono

--- src/main/java/com/spring/devplt/utils/GlobalControllerExceptionHandler.java
+++ src/main/java/com/spring/devplt/utils/GlobalControllerExceptionHandler.java
@@ -4,11 +4,14 @@ import com.spring.devplt.exceptions.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
 
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -19,29 +22,20 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(RestApiExceptions.class)
-    public @ResponseBody HttpErrorInfo handleResourceNotFoundExceptions(ServerHttpRequest request,final RestApiExceptions e){
+    public @ResponseBody
+    Mono<ResponseEntity<ErrorResponse>> handleResourceNotFoundExceptions(ServerHttpRequest request, final RestApiExceptions e){
         final ErrorCode errorCode = CommonErrorCode.RESOURCE_NOT_FOUND;
-        return createHttpErrorInfo(errorCode.getHttpStatus(),request,e);
+        return handleException(errorCode);
     }
 
-    @ResponseStatus(NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public @ResponseBody
-    HttpErrorInfo handleNotFoundExceptions(ServerHttpRequest request, Exception ex) {
-        return createHttpErrorInfo(NOT_FOUND, request, ex);
-    }//NotFoundException -> 404 NotFound 로 변환.
+    private ErrorResponse createErrorResponse(ErrorCode errorCode){
+        return ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage()).build();
+    }
 
-    @ResponseStatus(UNPROCESSABLE_ENTITY)
-    @ExceptionHandler(InvalidInputException.class)
-    public @ResponseBody HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, Exception ex) {
-        return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
-    }//InvalidInputException -> 422 UnProcessable Entity 로 변환.
-
-    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
-        final String path = request.getPath().pathWithinApplication().value();
-        final String message = ex.getMessage();
-
-        log.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
-        return new HttpErrorInfo(httpStatus, path, message);
+    private Mono<ResponseEntity<ErrorResponse>> handleException(ErrorCode errorCode){
+        return Mono.just(ResponseEntity.status(errorCode.getHttpStatus())
+                .body(createErrorResponse(errorCode)));
     }
 }
